d0d49c5a23018c0fb61a1ac374dde3e40e5e970c
Witek <kagglewitek@gmail.com>
2022-10-11 11:10:44 +0200

    copy from bitbucket
    skopiowane z bitbucket

---
+++ Ruby’s 2D Adventure/Assets/Scripts/RubyController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubyController : MonoBehaviour
+{
+    public float speed = 0.1f;
+    void Start()
+    {
+       // QualitySettings.vSyncCount = 0;
+      //  Application.targetFrameRate = 30;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector2 position = transform.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+        transform.position = position;
+
+
+       
+    }
+}
+
+
+
